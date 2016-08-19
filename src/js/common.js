7395278183ac23424f475cc3bc60d18ef76c34b0
sweelemk <sweelemk@gmail.com>
2016-08-19 16:49:03 +0300

    creare header, nav

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
 
-	// go js
+	// menu
+	function menuTrigger() {
+		var trigger = $(".navbar__wrap"),
+			menuContent = trigger.parent().find(".navbar__content"),
+			burger = trigger.find(".hamburger-inner"),
+			navbarText = trigger.find(".navbar_text-inner"),
+			textClose = navbarText.data("close"),
+			textOpen = navbarText.data("open");
+
+		trigger.on("click", function(){
+			if(!$(this).hasClass("open")) {
+				$(this).addClass("open");
+				menuContent.fadeIn({
+					duration: 150,
+					complete: function(){
+						$(this).addClass('open');
+						burger.addClass("open");
+						navbarText.find("span").text(textClose);
+					}
+				});
+			} else {
+				$(this).removeClass("open");
+				menuContent.removeClass('open').delay(300).fadeOut(150);
+				burger.removeClass("open");
+				navbarText.find("span").text(textOpen);
+			}
+		});
+
+		menuContent.on("click", function(){
+			trigger.removeClass("open");
+			menuContent.removeClass('open').delay(300).fadeOut(150);
+			burger.removeClass("open");
+			navbarText.find("span").text(textOpen);
+		});
+
+		menuContent.find(".navbar__content-slice").on("click", function(e){
+			e.stopPropagation();
+		});
+	} menuTrigger();
 
 });
