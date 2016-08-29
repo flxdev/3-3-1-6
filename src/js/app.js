102c189db15166e8dfc9b867ee133fda8fdaa648
sweelemk <sweelemk@gmail.com>
2016-08-29 14:48:09 +0300

    portfolio inner

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/perfect-scrollbar.min.js
+//=include lib/isotope.min.js
+//=include lib/lightgallery.min.js
 //=include common.js
