66afac40b9b6247bc5773bb9220762eabff28c69
sweelemk <sweelemk@gmail.com>
2016-09-01 09:50:49 +0300

    final page services

--- src/js/common.js
+++ src/js/common.js
@@ -367,50 +367,68 @@ $(document).ready(function() {
 
 	scroll3316();
 
-	function grayscale(src){
-		var canvas = document.createElement('canvas');
-		var ctx = canvas.getContext('2d');
-		var imgObj = new Image();
-		imgObj.src = src;
-		canvas.width = imgObj.width;
-		canvas.height = imgObj.height; 
-		ctx.drawImage(imgObj, 0, 0);
-		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-		for(var y = 0; y < imgPixels.height; y++){
-			for(var x = 0; x < imgPixels.width; x++){
-				var i = (y * 4) * imgPixels.width + x * 4;
-				var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-				imgPixels.data[i] = avg; 
-				imgPixels.data[i + 1] = avg; 
-				imgPixels.data[i + 2] = avg;
-			}
-		}
-		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-		return canvas.toDataURL();
-	};
+	function grayscale(){
 
-	//$(window).on("load", function(){
+		function setGrayscale(src){
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext('2d');
+			var imgObj = new Image();
+			imgObj.src = src;
+			canvas.width = imgObj.width;
+			canvas.height = imgObj.height; 
+			ctx.drawImage(imgObj, 0, 0);
+			var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			for(var y = 0; y < imgPixels.height; y++){
+				for(var x = 0; x < imgPixels.width; x++){
+					var i = (y * 4) * imgPixels.width + x * 4;
+					var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+					imgPixels.data[i] = avg; 
+					imgPixels.data[i + 1] = avg; 
+					imgPixels.data[i + 2] = avg;
+				}
+			}
+			ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+			return canvas.toDataURL();
+		};
+
+		var g = $(".greyscale"),
+			gFirst = g.parent().find('img:first'),
+			gImg = $('.img_grayscale');
+			
+		g.fadeIn(500);
 		
-		$(".greyscale").fadeIn(500);
-		
-		// clone image
-		$('.greyscale').each(function(){
+		g.each(function(){
 			var el = $(this);
 			el.css({"position":"absolute", "top": "50%", "bottom":"0"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute", "z-index":"2","opacity":"0"}).insertBefore(el).queue(function(){
 				var el = $(this);
 				el.dequeue();
 			});
-			this.src = grayscale(this.src);
+			this.src = setGrayscale(this.src);
 		});
 		
-		// Fade image 
-		$('.greyscale').mouseover(function(){
-			$(this).parent().find('img:first').stop().animate({opacity:1}, 300);
-		})
-		$('.img_grayscale').mouseout(function(){
-			$(this).stop().animate({opacity:0}, 300);
-		});		
-	//});
+		// g.mouseover(function(){
+		// 	// $(this).parent().find('img:first').stop().animate({opacity:1}, 300);
+		// 	TweenLite.to(gFirst, 0.5, {opacity: 1, ease: Power3.easeOut});
+		// })
+		// gImg.mouseout(function(){
+		// 	$(this).stop().animate({opacity:0}, 300);
+		// });
+		if($(".services-container").length) {
+			gHovers($(".service-container__inner"));
+		} else {
+			gHovers($(".greyscale"));
+		}		
+
+		function gHovers(item) {
+			item.mouseover(function(){
+				$(this).parent().find('img:first').stop().animate({opacity:1}, 300);
+			})
+			item.on("mouseout", function(){
+				$(this).parent().find(".img_grayscale").stop().animate({opacity:0}, 300);
+			});
+		}
+
+	} grayscale();
 
 
 	
